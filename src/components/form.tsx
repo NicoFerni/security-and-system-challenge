110ae0f85c04abd9552967fced852e80cb80a444
NicoFerni <nicoferni55@gmail.com>
2024-07-03 22:40:55 -0300

    doing the form, changing styles

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,28 +1,73 @@
+import React, { useState } from 'react'
 import styles from '@/styles/Form.module.css'
+
+interface FormData {
+    name: string;
+    email: string;
+    message: string
+}
+
 
 export function Form() {
 
+    const [sendText, setSendText] = useState('')
 
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        const formData: FormData = {
+            name: e.currentTarget.value,
+            email: e.currentTarget.value,
+            message: e.currentTarget.value,
+        }
+        setSendText('Mensaje enviado!')
+    }
 
+    console.log(onSubmit)
     return (
-        <form>
-            <div>
-                <h1>Contacto</h1>
-            </div>
-            <div>
-                <label>Nombre</label>
-                <input type="text" name="name" id="name" required />
+        <div className={styles.div}>
+            <h1 className={styles.text}>Contacto</h1>
+            <form onSubmit={onSubmit}>
+                <label className={styles.label}>
+                    Nombre
+                </label>
+                <input
+                    className={styles.input}
+                    placeholder='Ingrese su nombre'
+                    type='name'
+                    id='name'
+                    name='name'
+                />
 
-                <label>Email</label>
-                <input type="text" name="name" id="name" required />
+                <label className={styles.label}>
+                    Email
+                </label>
+                <input
+                    className={styles.input}
+                    placeholder='Ingrese un mail valido'
+                    
+                    id="email"
+                    name="email"
+                />
 
-                <label>Mensaje</label>
-                <input type="text" name="name" id="name" required />
-            </div>
-            <div>
-                <input type="submit" value="Enviar" />
-            </div>
-        </form>
+                <label className={styles.label}>
+                    Mensaje
+                </label>
+                <textarea
+                    className={styles.input}
+                    placeholder='Escriba su mensaje'
+                    id="message"
+                    name="message"
+                >
+
+                </textarea>
+
+                <button className={styles.button} type="submit">
+                    Enviar
+                </button>
+
+                {sendText && <p className={styles.sendText}>{sendText}</p>}
+            </form>
+        </div>
 
     )
 }
